c12993b17c6aeadf03a1910c92e3873e6a67fb30
Piyush Jaiswal <iampiyushjaiswal103@gmail.com>
2024-01-26 00:32:14 +0530

    feat(error): binded ErrorFilter

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ErrorFilter } from './shared/error.filter';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.useGlobalPipes(new ValidationPipe());
+    const httpAdapterHost = app.get(HttpAdapterHost);
+
+    app.useGlobalFilters(new ErrorFilter(httpAdapterHost));
     await app.listen(3000);
 }
 bootstrap();
